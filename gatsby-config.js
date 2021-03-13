55359cbddea06be29e682a2b270bffd6e7754acd
Jorge Ruvalcaba <jorgeruvalcabav@gmail.com>
2021-03-12 22:58:47 -0600

    Updating config file again

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,18 +9,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Drum Machine`,
-        short_name: `Drum Machine`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
